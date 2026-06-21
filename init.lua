f3173db48827fa90f54e292a1a1d7a5cfe9fa3d7
yvto <136810092+yvto@users.noreply.github.com>
2026-06-21 18:28:37 +0200

    Delete init.lua

--- init.lua
+++
@@ -1,9 +0,0 @@
-require("options")
-require("keymaps")
-
-require("config.lazy")
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { '<filetype>' },
-  callback = function() vim.treesitter.start() end,
-})
