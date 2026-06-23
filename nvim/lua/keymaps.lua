f1e4cbe201f124ac391914088397b8325b4427c0
yvto <136810092+yvto@users.noreply.github.com>
2026-06-23 18:04:06 +0200

    Delete nvim directory

--- nvim/lua/keymaps.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = " "
-
---Launching Neotree
-vim.keymap.set('n', '<C-n>', ':Neotree<CR>', { desc = "Open Neotree" })
-
--- vim.keymap.set('i', '<C-k', actions.move_selection_previous, {})
--- vim.keymap.set('i', '<C-j', actions.move_selection_next, {})
--- nvim/lua/plugins/colorizer.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "norcalli/nvim-colorizer.lua",
-
-  config = function()
-    return vim.api.nvim_command(table.concat(vim.iter({1, 2, 3, {2, 3, 10}}):flatten():totable(), " "))
-  end,
-}
